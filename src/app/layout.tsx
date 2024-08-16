3d1f1e12a59f5af89df99293e31193c431fdd4cf
satooru65536 <ryuto4619@gmail.com>
2024-08-17 00:25:13 +0900

    change: フォントを変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.scss';
+import { Sawarabi_Mincho } from 'next/font/google';
 import { Providers } from './Providers';
+import './globals.scss';
 
-const inter = Inter({ subsets: ['latin'] });
+const sawarabi = Sawarabi_Mincho({ weight: ['400'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
+      <body className={sawarabi.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
