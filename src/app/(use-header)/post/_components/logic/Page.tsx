cf3d082c13aca08cc3bd7ec491acca6a64d29de0
nishi240masa <masa2401106@gmail.com>
2024-10-01 16:16:02 +0900

    add:tagStringの追加

--- src/app/(use-header)/post/_components/logic/Page.tsx
+++ src/app/(use-header)/post/_components/logic/Page.tsx
@@ -1,48 +1,58 @@
 import { useState } from 'react';
 import Page from '../view/Page';
+import { TablesInsert } from '@/types/supabase';
+import { PostLanguage, PostTag } from '@/types/post';
 
-interface Post {
-  title: string;
-  description: string;
-  tags: string[];
-  code: string;
-  language: string;
-}
+type Post = Partial<TablesInsert<'post'>>;
 
 export default function PostPageLogic() {
-  const [post, setPost] = useState<Post>({
-    title: '',
-    description: '',
-    tags: [],
-    code: '',
-    language: '',
-  });
+  const [post, setPost] = useState<Post>({});
+  const [tag, setTag] = useState<PostTag[]>();
+  const [tagString, setTagString] = useState<string>('');
+  const languages = [{ id: 1, name: 'ts' }] satisfies PostLanguage[];
 
-  // titleをセットする関数
+  //codeのセット
+  function setCode(code: string) {
+    setPost((prev) => ({ ...prev, code }));
+  }
+
+  // descriptionのセット
+  function setDescription(description: string) {
+    setPost((prev) => ({ ...prev, description }));
+  }
+
+  // lang_idのセット
+  function setLangId(lang_id: number) {
+    setPost((prev) => ({ ...prev, lang_id }));
+  }
+
+  // titleのセット
   function setTitle(title: string) {
     setPost((prev) => ({ ...prev, title }));
   }
 
-  // descriptionをセットする関数
-  function setDescription(description: string) {
-    setPost((prev) => ({ ...prev, description }));
-  }
-  // languageをセットする関数
-  function setTags(tags: string[]) {
-    setPost((prev) => ({ ...prev, tags }));
+  // user_uidのセット
+  function setUserUid(user_uid: string) {
+    setPost((prev) => ({ ...prev, user_uid }));
   }
 
-  // tagをセットする関数
-  function setCode(code: string) {
-    setPost((prev) => ({ ...prev, code }));
+  // tagsのセット
+  function setTags(tags: PostTag[]) {
+    setTag(tags);
   }
-  // languageをセットする関数
-  function setLanguage(language: string) {
-    setPost((prev) => ({ ...prev, language }));
+
+  // コンマ区切りのタグを配列に変換
+  function changeToArray(tagString: string) {
+    const tags = tagString.split(',');
+    const postTags = tags.map((tag) => ({ tag }));
+    console.log(postTags); // eslint-disable-line no-console
   }
 
   // 投稿処理
   function submit() {
+    // タグの文字列を配列に変換
+    changeToArray(tagString);
+
     // ここで投稿処理を行う
     console.log(post); // eslint-disable-line no-console
   }
@@ -50,11 +60,17 @@ export default function PostPageLogic() {
   return (
     <Page
       post={post}
-      setTitle={setTitle}
-      setDescription={setDescription}
-      setTags={setTags}
+      tag={tag}
+      language={languages}
       setCode={setCode}
+      setDescription={setDescription}
+      setLangId={setLangId}
+      setTitle={setTitle}
+      setUserUid={setUserUid}
+      setTags={setTags}
       submit={submit}
+      tagString={tagString}
+      setTagString={setTagString}
     />
   );
 }
--- src/app/(use-header)/post/_components/view/Page/index.tsx
+++ src/app/(use-header)/post/_components/view/Page/index.tsx
@@ -1,39 +1,51 @@
 'use client';
 
-import { useState } from 'react';
+import { PostLanguage, PostTag } from '@/types/post';
+import { TablesInsert } from '@/types/supabase';
 import styles from './index.module.scss';
+import { Dispatch, SetStateAction } from 'react';
 
+type Post = Partial<TablesInsert<'post'>>;
+type tag = { tags: string[] };
 interface PageProps {
-  post: {
-    title: string;
-    description: string;
-    tags: string[];
-    code: string;
-  };
-  setTitle: (title: string) => void;
-  setDescription: (description: string) => void;
-  setTags: (tags: string[]) => void;
+  post: Post;
+  tag: PostTag[] | undefined;
+  language: PostLanguage[];
   setCode: (code: string) => void;
+  setDescription: (description: string) => void;
+  setLangId: (lang_id: number) => void;
+  setTitle: (title: string) => void;
+  setUserUid: (user_uid: string) => void;
+  setTags: (tags: PostTag[]) => void;
   submit: () => void;
+  tagString: string;
+  setTagString: Dispatch<SetStateAction<string>>;
 }
 
-export default function Page({ post, setTitle, setDescription, setTags, setCode, submit }: PageProps) {
-  const [title, _setTitle] = useState(''); // 本来は title, setTitle は引数から受け取る
-  const [description, _setDescription] = useState('');
-  const [language, setLanguage] = useState('');
-  const [tag, _setTag] = useState('');
-  const [code, _setCode] = useState('');
-
+export default function Page({
+  post,
+  tag,
+  language,
+  setCode,
+  setDescription,
+  setLangId,
+  setTitle,
+  setUserUid,
+  setTags,
+  submit,
+  tagString,
+  setTagString,
+}: PageProps) {
   return (
     <div>
       <div className={styles.container}>
         <div className={styles.card}>
           <p>Title</p>
-          <input placeholder="タイトルを入力してください" type="text" value={title} />
+          <input placeholder="タイトルを入力してください" type="text" value={post.title} />
         </div>
         <div className={styles.card}>
           <p>Description</p>
-          <input placeholder="説明を入力してください" type="text" value={description} />
+          <input placeholder="説明を入力してください" type="text" value={post.description} />
         </div>
         <div className={styles.card}>
           <p>Language</p>
@@ -44,18 +56,26 @@ export default function Page({ post, setTitle, setDescription, setTags, setCode,
             }}
             value={language}
           >
+            {/* ここにlanguageのIDを入れる,文字のところにはnameを入れる */}
             <option value="">言語を選択してください</option>
             {/* ここに言語の選択肢を追加していく */}
           </select>
         </div>
         <div className={styles.card}>
           <p>Tags</p>
-          <input placeholder="タグを入力してください" type="text" value={tag} />
+          <input
+            placeholder="タグを入力してください"
+            type="text"
+            value={tagString}
+            onChange={(e) => {
+              setTagString(e.target.value);
+            }}
+          />
         </div>
       </div>
       <div className={styles.codeArea}>
         <p>Kuso Code</p>
-        <textarea name="code">{code}</textarea>
+        <textarea name="code">{post.code}</textarea>
       </div>
     </div>
   );
