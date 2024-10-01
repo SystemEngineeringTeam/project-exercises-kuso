c49e685d5c40039f474581f8f2994d7ccee61aa9
kanakanho <kahoshibakichi@gmail.com>
2024-10-01 16:03:53 +0900

    fix: supabaseからの読み取りを簡略化

--- src/stores/postAtom.ts
+++ src/stores/postAtom.ts
@@ -1,67 +1,83 @@
 import { atom } from 'jotai';
-import { type PostLanguage, type Post, type User, type PostTag } from '@/types/post';
+import { type PostTag, type Post, type User } from '@/types/post';
 import { supabase } from '@/utils/supabase/client';
 
+interface ResPost {
+  code: string;
+  description: string;
+  id: number;
+  lang_id: number;
+  title: string;
+  user_uid: string;
+  language: {
+    id: number;
+    name: string;
+  } | null;
+  user: User | null;
+  post_tag: Array<{
+    id: number;
+    tag: string;
+  }> | null;
+  crazy_score: Array<{
+    score: number;
+  }> | null;
+}
+
 const fetchUser = async (): Promise<Post[]> => {
-  const resPosts = await supabase.from('post').select();
+  const resPosts = await supabase.from('post').select(`
+    *,
+    language (
+      id,
+      name
+    ),
+    user (
+      id,
+      uid,
+      name
+    ),
+    post_tag (
+      id,
+      tag
+    ),
+    crazy_score (
+      score
+    )
+  `);
 
   if (resPosts.data === null) {
     throw new Error('Error fetching data');
   }
 
-  const res = await Promise.all(
-    resPosts.data.map(
-      async (resPost: {
-        code: string;
-        description: string;
-        id: number;
-        lang_id: number;
-        title: string;
-        user_uid: string;
-      }) => {
-        const resLangs = await supabase.from('language').select('id, name');
-        const resPostTags = await supabase.from('post_tag').select('post_id, tag').eq('post_id', resPost.id);
-        const resCrasyScores = await supabase.from('crazy_score').select('post_id, score').eq('post_id', resPost.id);
-        const resUsers = await supabase.from('user').select().eq('uid', resPost.user_uid);
+  return resPosts.data.map((resPost: ResPost) => {
+    if (
+      resPost.language === null ||
+      resPost.user === null ||
+      resPost.post_tag === null ||
+      resPost.crazy_score === null
+    ) {
+      throw new Error('Error fetching data');
+    }
 
-        if (
-          resLangs.data === null ||
-          resPostTags.data === null ||
-          resCrasyScores.data === null ||
-          resUsers.data === null
-        ) {
-          throw new Error('Error fetching data');
-        }
+    const averageCrazyScore =
+      resPost.crazy_score.reduce((acc: number, cur: { score: number }) => acc + cur.score, 0) /
+      resPost.crazy_score.length;
 
-        const language = resLangs.data.find(
-          (resLang: { id: number }) => resLang.id === resPost.lang_id,
-        ) as PostLanguage;
+    const postTags = resPost.post_tag.map((tag) => ({
+      ...tag,
+      post_id: resPost.id,
+    })) as PostTag[];
 
-        const tags = resPostTags.data.filter(
-          (resPostTag: { post_id: number }) => resPostTag.post_id === resPost.id,
-        ) as PostTag[];
-
-        const averageCrazyScore =
-          resCrasyScores.data.reduce((acc: number, cur: { score: number }) => acc + cur.score, 0) /
-          resCrasyScores.data.length;
-
-        const user = resUsers.data.find((resUser: { uid: string }) => resUser.uid === resPost.user_uid) as User;
-
-        return {
-          id: resPost.id,
-          code: resPost.code,
-          description: resPost.description,
-          title: resPost.title,
-          crazy_score: averageCrazyScore,
-          post_tags: tags,
-          user,
-          language,
-        };
-      },
-    ),
-  );
-
-  return res;
+    return {
+      id: resPost.id,
+      code: resPost.code,
+      description: resPost.description,
+      title: resPost.title,
+      crazy_score: averageCrazyScore,
+      post_tags: postTags,
+      user: resPost.user,
+      language: resPost.language,
+    };
+  });
 };
 
 export const postAtom = atom(async () => await fetchUser());
