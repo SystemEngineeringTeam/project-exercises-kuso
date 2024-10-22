227faf3013fe7292b30e08a5c89151bcf68b283b
satooru65536 <ryuto4619@gmail.com>
2024-10-22 13:12:11 +0900

    change: 表記を変更

--- supabase/migrations/20241022040643_set-rls.sql
+++ supabase/migrations/20241022040643_set-rls.sql
@@ -6,7 +6,7 @@ create policy "全ユーザ読み込み可" on "public"."crazy_score" as permiss
 select
     to public using (true);
 
-create policy "認証済みのみ書き込み可能" on "public"."crazy_score" as permissive for insert to authenticated
+create policy "認証済みのみ書き込み可ï" on "public"."crazy_score" as permissive for insert to authenticated
 with
     check (true);
 
@@ -14,7 +14,7 @@ create policy "全ユーザ読み込み可" on "public"."language" as permissive
 select
     to public using (true);
 
-create policy "認証済みのみ書き込み可能" on "public"."language" as permissive for insert to authenticated
+create policy "認証済みのみ書き込み可ï" on "public"."language" as permissive for insert to authenticated
 with
     check (true);
 
@@ -22,11 +22,11 @@ create policy "全ユーザ読み込み可" on "public"."post" as permissive for
 select
     to public using (true);
 
-create policy "認証済みのみ書き込み可能" on "public"."post" as permissive for insert to authenticated
+create policy "認証済みのみ書き込み可ï" on "public"."post" as permissive for insert to authenticated
 with
     check (true);
 
-create policy "全ユーザ取得可能" on "public"."post_tag" as permissive for
+create policy "全ユーザ取得可ï" on "public"."post_tag" as permissive for
 select
     to public using (true);
 
@@ -34,10 +34,10 @@ create policy "認証済みのみ書き込み可" on "public"."post_tag" as perm
 with
     check (true);
 
-create policy "全ユーザ取得可能" on "public"."user" as permissive for
+create policy "全ユーザ取得可ï" on "public"."user" as permissive for
 select
     to public using (true);
 
-create policy "認証済みのみ書き込み可能" on "public"."user" as permissive for insert to authenticated
+create policy "認証済みのみ書き込み可ï" on "public"."user" as permissive for insert to authenticated
 with
     check (true);
