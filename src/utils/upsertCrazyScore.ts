15214ba493a9890b916e9b03935c99bfdec1fe13
kanakanho <kahoshibakichi@gmail.com>
2024-11-05 17:24:49 +0900

    fix: エラーの内容を修正

--- src/utils/upsertCrazyScore.ts
+++ src/utils/upsertCrazyScore.ts
@@ -4,10 +4,16 @@ import type { Tables, TablesInsert } from '@/types/supabase';
 const upsertCrazyScore = async (
   table: Omit<TablesInsert<'crazy_score'>, 'id' | 'created_at' | 'deleted_at'>,
 ): Promise<Tables<'crazy_score'> | undefined> => {
-  const { data } = await supabase.from('crazy_score').upsert(table).select('*');
+  const { data, error } = await supabase.from('crazy_score').upsert(table).select('*');
 
+  if (error !== null) {
+    throw new Error(error.message);
+  }
   if (data === null) {
-    throw new Error('Error inserting data');
+    throw new Error('Error return data is null');
+  }
+  if (data.length === 0) {
+    throw new Error('Error return data length is 0');
   }
 
   return data[0];
